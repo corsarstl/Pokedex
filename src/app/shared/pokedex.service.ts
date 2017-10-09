ba707e63c48895404f71885ce40fdb0a059df1d8
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-10-09 13:00:45 +0300

    pokemon-details is loading to console

--- src/app/shared/pokedex.service.ts
+++ src/app/shared/pokedex.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Input } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
 import { Pokemon } from '../pokemon';
+import { PokemonDetails } from '../pokemon-details';
 
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/observable/throw';
@@ -13,28 +13,12 @@ import 'rxjs/add/operator/catch';
 export class PokedexService {
 
   @Input() baseImageUrl: string = 'https://pokeapi.co/media/img/';
-  // pokemons = {};
-  private baseUrl: string = 'https://pokeapi.co/api/v1/pokemon/?limit=3';
-
-
+  private baseUrl: string = 'https://pokeapi.co/api/v1/pokemon/';
 
   constructor(private http: Http) { }
 
-  // getAllPokemons (): Promise<any> {
-  //   return this.http.get(`${this.baseUrl}`)
-  //     .toPromise()
-  //     .then(response => response.json().results)
-  // }
-
-  // getAllPokemons(): Observable<Pokemon[]>  {
-  //   return this.http.get(`${this.baseUrl}`)
-  //     .map((response: Response) => <Pokemon[]> response.json())
-  //     .do(data => )
-  //     .catch(this.handleError);
-  // }
-
   getAllPokemons() {
-    return this.http.get(`${this.baseUrl}`)
+    return this.http.get(`${this.baseUrl}?limit=12`)
       .toPromise()
       .then((res: Response) => {
         let data = res.json();
@@ -62,7 +46,34 @@ export class PokedexService {
   }
 
   getOnePokemon(selectedPokemon) {
-    console.log(selectedPokemon.id);
+    return this.http.get(`${this.baseUrl}${selectedPokemon.id}`)
+      .toPromise()
+      .then((res: Response) => {
+        let data = res.json();
+        let pokemonDetails = new PokemonDetails();
+
+        pokemonDetails.number = data.national_id;
+        pokemonDetails.name = data.name;
+        pokemonDetails.types = [];
+        pokemonDetails.types.push(data.types[0].name);
+
+        if (data.types[1] !== undefined) {
+          pokemonDetails.types.push(data.types[1].name);
+        }
+
+        pokemonDetails.attack = data.attack;
+        pokemonDetails.defense = data.defense;
+        pokemonDetails.hp = data.hp;
+        pokemonDetails.spAttack = data.sp_atk;
+        pokemonDetails.spDefense = data.sp_def;
+        pokemonDetails.speed = data.speed;
+        pokemonDetails.weight = data.weight;
+        pokemonDetails.totalMoves = data.moves.length;
+        console.log(pokemonDetails);
+
+        return pokemonDetails;
+      })
+      .catch (this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
