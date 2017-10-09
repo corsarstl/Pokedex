379eb36f1e1e245d98aaebe735d383a86727ecbe
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-10-09 10:43:51 +0300

    add selection of pokemon

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -10,6 +10,10 @@ import { Pokemon } from '../pokemon';
 export class PokemonListComponent implements OnInit {
 
   pokemons: Pokemon[];
+  public selectedPokemon = Pokemon;
+  // public isSelected = false;
+  // selectedIndex: number;
+
   // errorMessage: string;
 
   constructor(private pokedexService: PokedexService) { }
@@ -21,6 +25,19 @@ export class PokemonListComponent implements OnInit {
         console.log(pokemons);
       });
   }
+
+  onSelect(pokemon): void {
+    this.selectedPokemon =  pokemon;
+  }
+
+  // onSelect(pokemon) {
+  //   this.selectedPokemon = pokemon;
+  //   this.isSelected = true;
+  // }
+  // public pokemonSelect(index: number) {
+  //   this.selectedIndex = index;  // don't forget to update the model here
+  //   // ... do other stuff here ...
+  // }
 
   // getPokemons() {
   //   this.pokedexService.getAllPokemons()
