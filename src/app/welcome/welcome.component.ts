efafd5f2e96d938b095dda377751a57a2545c969
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-10-10 10:02:58 +0300

    add some responsive design

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pok-welcome',
   templateUrl: './welcome.component.html',
-  // styleUrls: ['./welcome.component.css']
-  styles: []
+  styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
 }
